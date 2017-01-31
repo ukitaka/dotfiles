30cc30854106fe66118a6205d70d8021654874ae
ukitaka <yuki.takahashi.1126@gmail.com>
2017-01-31 23:17:04 +0900

    Check if symbolic link

--- setup.sh
+++ setup.sh
@@ -8,9 +8,11 @@ fi
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    if [ ! -e $HOME/$file ]; then
+        ln -s $HOME/dotfiles/$file $HOME/$file
+    fi
 done
 
 [ ! -d ~/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
-git config --global core.excludesfile ~/.gitignore
+
