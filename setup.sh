53c7a0d8d64de1a3a50e84d8820c56bd9d655f40
ukitaka <yuki.takahashi.1126@gmail.com>
2017-01-31 23:18:15 +0900

    Use zplug instead of oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,6 @@ do
     fi
 done
 
-[ ! -d ~/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[ ! -d ~/.zplug ] && curl -sL zplug.sh/installer | zsh
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
 
